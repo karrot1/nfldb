d94d14c41fa4e5e7593cd96703ca185b83f922e0
Khalil Sheehan-Miles <sheehanmilesk@wit.edu>
2018-07-29 16:44:00 -0400

    more checking

--- phpcodetry2.php
+++ phpcodetry2.php
@@ -32,14 +32,16 @@ while($row = $result->fetch_object())
 $tempArray = $row;
   //error_log(array_column($tempArray, 0));
     array_push($resultArray, $tempArray);
+
+    error_log(mysqli_error($con));
 }
 
-
-error_log(mysqli_error($con));
 // Encode the array to JSON and output the results
 //echo "Hey, testing to see if echo works at all with this awful thing";
 //var_dump(json_encode($resultArray));
 //print_r(json_encode($resultArray));
+print_r( $resultArray );
+var_dump( $resultArray );
 echo json_encode($resultArray);
 }
 
